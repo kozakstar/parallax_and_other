74241e5447cc855042eb574d22be9b6334b651b8
kozakstar <kozakstar1991@gmail.com>
2018-03-23 20:06:48 +0200

    паралах на jquery

---
+++ parallax/main.js
@@ -0,0 +1,4 @@
+$('.parallax-window').parallax({imageSrc: 'img/1.jpg'});
+($('.parallax--one')).parallax({imageSrc: 'img/2.jpg'});
+($('.parallax--two')).parallax({imageSrc: 'img/3.jpg'});
+($('.parallax--three')).parallax({imageSrc: 'img/4.jpg'});
